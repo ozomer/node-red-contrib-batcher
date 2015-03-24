17e169622a2e443886d9e09ba8d23a21e3bf2480
Oren Zomer <oren.zomer@gmail.com>
2015-03-24 15:13:19 +0200

    chunkifier v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # node-red-contrib-chunkifier
 Node-RED node that collects sequences of payloads and send them in a single message.
+
+
+Collects message payloads by topic and sends them in chunk (array) when:
+
+* The number of messages in the given topic reaches the configured maximal value.
+* The number of unique topics in cache crosses the configured limit (flushes the topic of the oldest message in the cache).
+* A message with a non-true payload (false, null, undefined, empty-string...) arrives. If the topic is non-true, all the cache will be flushed, otherwise only the given topic will be flushed.
+* The oldest message in cache of a given topic waits for more than the maximal delay time (use a negative value to disable feature).
---
+++ chunkifier.js
@@ -0,0 +1,105 @@
+/**
+ * Copyright 2015 Awear Solutions Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+  "use strict";
+
+  RED.nodes.registerType("collector", function ChunkifierNode(n) {
+    RED.nodes.createNode(this,n);
+    this.name = n.name;
+    this.maxTopics = Math.max(1, parseInt(n.maxTopics) || 0);
+    this.maxMessagesPerTopic = Math.max(1, parseInt(n.maxMessagesPerTopic) || 0);
+    this.maxDelay = (n.maxDelay * 1) || 0;
+
+    this.topicsCount = 0;
+    this.topics = {};
+
+    var node = this;
+
+    function flushTopic(topic) {
+      if (!(topic in node.topics)) {
+        return;
+      }
+      if (node.topics[topic].timeout) {
+        clearTimeout(node.topics[topic].timeout);
+      }
+      node.topicsCount--;
+      node.send({
+        "topic": topic.slice(1), // remove '#'
+        "payload": node.topics[topic].payloads
+      });
+      delete node.topics[topic];
+    }
+
+    function flushAllTopics() {
+      for (var existingTopic in node.topics) {
+        if (existingTopic[0] == '#') {
+          flushTopic(existingTopic);
+        }
+      }
+    }
+
+    this.on("input", function(msg) {
+      // Save topics with a leading '#' to avoid javascript internals
+      // (for example, a topic named "hasOwnProperty")
+      var topic = '#' + ((msg.topic)?(msg.topic):'');
+
+      if (msg.payload) {
+        // Add msg.payload
+        if (!(topic in this.topics)) {
+          this.topics[topic] = {
+            "payloads": []
+          };
+          if (this.maxDelay >= 0) {
+            var topicObject = this.topics[topic];
+            this.topics[topic].timeout = setTimeout(function() {
+              // Safety check - topic object not replaced.
+              if (topicObject == this.topics[topic]) {
+                delete topicObject.timeout;
+                flushTopic(topic);
+              }
+            }, this.maxDelay);
+          }
+          this.topicsCount++;
+          if (this.topicsCount > this.maxTopics) {
+            for (var oldestTopic in this.topics) {
+              if (oldestTopic[0] == '#') {
+                flushTopic(oldestTopic);
+                break;
+              }
+            }
+          }
+        }
+        this.topics[topic].payloads.push(msg.payload);
+        if (this.topics[topic].payloads.length >= this.maxMessagesPerTopic) {
+          flushTopic(topic);
+        }
+      } else {
+        // flush topic
+        if (topic != '#') {
+          flushTopic(topic);
+        } else {
+          flushAllTopics();
+        }
+      }
+    });
+
+    this.on("close", function() {
+      flushAllTopics();
+    });
+
+  });
+};
